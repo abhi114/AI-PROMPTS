fc098658ed40ba1f6d9db1f25c56dfc674205eb7
abhi114 <nabhi531@gmail.com>
2023-08-21 20:27:17 +0530

    initial

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Feed from "@components/Feed";
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover And Share
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">Ai- Powered Prompts</span>
+        
+      </h1>
+      <p className="desc text-center">Promtopia Is An Open-Source Tool for modern world to discover , share and create creative prompts</p>
+      <Feed/>
+    </section>
+  )
+}
+
+export default Home;
